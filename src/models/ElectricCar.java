0bbd7a608c80a5cd05621f2fd1720cc460e1064c
Tommy Condon <tcondon12@icloud.com>
2023-03-29 11:31:50 +0100

    Comment out CarbonFuelCar.java and ElectricCar.java constructors

--- src/models/ElectricCar.java
+++ src/models/ElectricCar.java
@@ -1,11 +1,11 @@
 package models;
 
-public class ElectricCar extends Car {
-    public ElectricCar(String elec5678, String s, int i, Manufacturer kia, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
-    }
+// public class ElectricCar extends Car {
+  //  public ElectricCar(String elec5678, String s, int i, Manufacturer kia, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
+  //  }
 
-    @Override
-    public double getCarbonFootPrint() {
-        return 0;
-    }
-}
+  //  @Override
+  //  public double getCarbonFootPrint() {
+  //      return 0;
+  //  }
+// }
