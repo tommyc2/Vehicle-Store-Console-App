8259bf38dbac1fd4f21ce9151e81364cacff1f94
Tommy Condon <tcondon12@icloud.com>
2023-04-04 16:58:11 +0100

    Boiler plate code for CarbonFuelCarTest

---
+++ test/models/CarbonFuelCarTest.java
@@ -0,0 +1,70 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarbonFuelCarTest {
+
+    CarbonFuelCar carbonFuelCarValidAtLowValue, carbonFuelCarInvalidBelowLowValue, carbonFuelCarValidAtHighValue, carbonFuelCarInvalidAboveHighValue;
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Nested
+    class constructorTestsForCarbon {
+    @Test
+        void validatingFuelConsumption(){
+
+        }
+        @Test
+        void validatingCarbonEmission(){
+
+        }
+
+        @Test
+        void validatingIfItsAutomaticOrNot(){
+
+        }
+
+        @Test
+        void validatingFuelType(){
+
+        }
+
+        @Test
+        void validatingEngineSize(){
+
+        }
+    }
+
+    @Nested
+    class setterAndGetterTestsCarbon {
+
+        @Test
+        void setFuelConsumption() {
+        }
+
+        @Test
+        void setCarbonEmission() {
+        }
+
+        @Test
+        void setAutomatic() {
+        }
+
+        @Test
+        void setFuelType() {
+        }
+
+        @Test
+        void setEngineSize() {
+        }
+    }
+
+    @Test
+    void testToString() {
+    }
+}
